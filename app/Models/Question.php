b5a5f67705c2e91a805a68f7cf0911fe70629b8f
Felipe Ramos <felipe.ramos@idx.digital>
2024-08-05 09:23:14 -0300

    HEY-11: creating seeder and factory for question model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,6 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Question extends Model {}
+class Question extends Model
+{
+    use HasFactory;
+}
