c437a1490f09f8c7eff2f0af621f49b02a9d365c
Felipe Ramos <felipe.ramos@idx.digital>
2024-08-05 10:24:49 -0300

    HEY-11: creating the frontend of our form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,11 +7,47 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+            <form action="{{ route('question.store') }}" method="POST">
+                @csrf
+                <div class="mb-4">
+                    <label for="question" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        Question
+                    </label>
+                    <textarea
+                        name="question"
+                        id="question"
+                        rows="4"
+                        class="
+                            block p-2.5 w-full text-sm text-gray-900 bg-gray-50
+                            rounded-lg border border-gray-300 focus:ring-blue-500
+                            focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600
+                            dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
+                            dark:focus:border-blue-500" placeholder="Ask me anything...">{{ old('question') }}</textarea>
+                    @error('question')
+                    <span class="text-red-400">{{ $message }}</span>
+                    @enderror
                 </div>
-            </div>
-        </div>
+                <button
+                    type="submit"
+                    class="
+                        text-white bg-blue-700 hover:bg-blue-800
+                        focus:outline-none focus:ring-4 focus:ring-blue-300
+                        font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2
+                        dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                    Save
+                </button>
+                <button
+                    type="reset"
+                    class="
+                        py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white
+                        rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10
+                        focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800
+                        dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                    >
+                    Cancel
+                </button>
+            </form>
+
     </div>
 </x-app-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
+        './node_modules/flowbite/**/*.js',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+
     ],
 
     theme: {
@@ -17,5 +20,8 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        require('flowbite/plugin')
+    ],
 };
